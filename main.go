005c6f19848fd667fea0a1938801ec0bbd015e69
azusachino <azusa146@gmail.com>
2022-11-30 17:04:03 +0800

    [feat] how to instruct k8s through curl

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"path"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	// 模拟目标Objects的标签
+	// 模拟目标 Objects 的标签
 	lbls := labels.Set{"node": "minikube", "app": "mysql"}
 
 	selector := labels.NewSelector()
@@ -37,4 +43,14 @@ func main() {
 	} else {
 		panic("Selector should have matched labels")
 	}
+
+	// list pods
+	home := homedir.HomeDir()
+	config, _ := clientcmd.BuildConfigFromFlags("", path.Join(home, ".kube/config"))
+	client, _ := kubernetes.NewForConfig(config)
+
+	// access the api to list pods
+	pods, _ := client.CoreV1().Pods("default").List(context.Background(), v1.ListOptions{})
+
+	fmt.Printf("Pods count: %d\n", len(pods.Items))
 }
